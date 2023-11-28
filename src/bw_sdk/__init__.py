bcc3b48c1c2ce1ecde297388b7b2833ccf614810
Kalle M. Aagaard <git@k-moeller.dk>
2023-11-28 10:40:33 +0100

    enum

--- src/bw_sdk/__init__.py
+++ src/bw_sdk/__init__.py
@@ -9,8 +9,9 @@ from pydantic import SecretStr as SecretStr
 from pydantic import TypeAdapter
 
 import bw_sdk.model as _m
+from bw_sdk.model import DBStatus, LinkTarget, Match
 
-T = TypeVar('T')
+T = TypeVar("T")
 
 
 type RespT[T] = Annotated[
@@ -30,10 +31,10 @@ type ListRespT[T] = Annotated[
 
 StrResp = pydantic.TypeAdapter(RespT[_m.StrObj])
 
-UnlockResp =  pydantic.TypeAdapter(RespT[_m.UnlockData])
-LockResp =  pydantic.TypeAdapter(RespT[_m.MessageObj])
-SyncResp =  pydantic.TypeAdapter(RespT[_m.MessageObj])
-StatusResp =  pydantic.TypeAdapter(TmplRespT[_m.ServerStatus])
+UnlockResp = pydantic.TypeAdapter(RespT[_m.UnlockData])
+LockResp = pydantic.TypeAdapter(RespT[_m.MessageObj])
+SyncResp = pydantic.TypeAdapter(RespT[_m.MessageObj])
+StatusResp = pydantic.TypeAdapter(TmplRespT[_m.ServerStatus])
 
 OrgResp = pydantic.TypeAdapter(RespT[_m.Organization])
 OrgsResp = pydantic.TypeAdapter(ListRespT[_m.Organization])
@@ -49,7 +50,6 @@ NewItem = _m.NewItemLogin | _m.NewItemSecureNote
 BaseObjT = TypeVar("BaseObjT", bound=_m.BaseObj)
 
 
-
 @dataclasses.dataclass
 class Client:
     http_client: httpx.Client = dataclasses.field(
@@ -58,12 +58,18 @@ class Client:
 
     # region Internal
 
-    def _put(self, validator: TypeAdapter[RespT[T]], path: str, params: _m.Query | None, payload: _m.Payload | _m.BaseObj | None):
-        _params = None if params is None else params.model_dump(mode='json', by_alias=True, exclude_none=True)
+    def _put(
+        self,
+        validator: TypeAdapter[RespT[T]],
+        path: str,
+        params: _m.Query | None,
+        payload: _m.Payload | _m.BaseObj | None,
+    ):
+        _params = None if params is None else params.model_dump(mode="json", by_alias=True, exclude_none=True)
         _payload = None if payload is None else payload.model_dump(mode="json", by_alias=True)
-        
+
         res = self.http_client.put(path, params=_params, json=_payload)
-        
+
         resp = validator.validate_json(res.content)
 
         if isinstance(resp, _m.ErrorResponse):
@@ -72,29 +78,35 @@ class Client:
         return resp.data
 
     def _post(self, path: str, params: _m.Query | None, payload: _m.Payload | _m.BaseObj | None):
-        _params = None if params is None else params.model_dump(mode='json', by_alias=True, exclude_none=True)
+        _params = None if params is None else params.model_dump(mode="json", by_alias=True, exclude_none=True)
         _payload = None if payload is None else payload.model_dump(mode="json", by_alias=True)
 
         res = self.http_client.post(path, params=_params, json=_payload)
         return res
-    
-    def _post_object(self, validator: TypeAdapter[RespT[T]], path: str, params: _m.Query | None, payload: _m.Payload | _m.BaseObj | None):
+
+    def _post_object(
+        self,
+        validator: TypeAdapter[RespT[T]],
+        path: str,
+        params: _m.Query | None,
+        payload: _m.Payload | _m.BaseObj | None,
+    ):
         res = self._post(path, params, payload)
-        
+
         resp = validator.validate_json(res.content)
 
         if isinstance(resp, _m.ErrorResponse):
             raise Exception(f"Could not get obj [{resp.message}]")
 
         return resp.data
-    
+
     def _delete(self, path: str, params: _m.Query | None):
-        _params = None if params is None else params.model_dump(mode='json', by_alias=True, exclude_none=True)
+        _params = None if params is None else params.model_dump(mode="json", by_alias=True, exclude_none=True)
         res = self.http_client.delete(path, params=_params)
         res.raise_for_status()
-        
+
     def _get(self, validator: TypeAdapter[RespT[T]], path: str, params: _m.Query | None):
-        _params = None if params is None else params.model_dump(mode='json', by_alias=True, exclude_none=True)
+        _params = None if params is None else params.model_dump(mode="json", by_alias=True, exclude_none=True)
 
         res = self.http_client.get(path, params=_params)
         resp = validator.validate_json(res.content)
@@ -104,15 +116,17 @@ class Client:
 
         return resp.data
 
-    def _get_str(self, path: str, params: _m.Query | None) -> str:        
+    def _get_str(self, path: str, params: _m.Query | None) -> str:
         return self._get(StrResp, path, params).data
-   
-    def _get_object(self, validator: TypeAdapter[RespT[BaseObjT]], obj_type: str, obj_id: str, params: _m.Query | None) -> BaseObjT:
+
+    def _get_object(
+        self, validator: TypeAdapter[RespT[BaseObjT]], obj_type: str, obj_id: str, params: _m.Query | None
+    ) -> BaseObjT:
         return self._get(validator, f"/object/{obj_type}/{obj_id}", params)
-           
+
     def _get_tmpl(self, validator: TypeAdapter[TmplRespT[T]], path: str, params: _m.Query | None) -> T:
         return self._get(validator, path, params).template
-    
+
     def _get_list(
         self,
         validator: TypeAdapter[ListRespT[T]],
@@ -129,9 +143,9 @@ class Client:
         exact: bool,
     ) -> list[BaseObjT]:
         search = None if params is None else params.search
-        
+
         path = f"/list/object/{obj_type}"
-        
+
         result = self._get_list(validator, path, params)
 
         if exact:
@@ -143,17 +157,15 @@ class Client:
     # region Misc
 
     def unlock(self, password: SecretStr):
-        payload = _m.UnlockPayload(
-            password=password
-        )
+        payload = _m.UnlockPayload(password=password)
         return self._post_object(UnlockResp, "/unlock", params=None, payload=payload)
 
     def lock(self):
-        return self._post_object(LockResp, "/lock", params=None, payload=None)        
+        return self._post_object(LockResp, "/lock", params=None, payload=None)
 
     def sync(self):
         return self._post_object(SyncResp, "/sync", params=None, payload=None)
-        
+
     def get_status(self):
         return self._get_tmpl(StatusResp, "/status", None)
 
@@ -167,25 +179,24 @@ class Client:
     def get_item(self, item: _m.Item | _m.ItemID):
         obj_id = item if isinstance(item, str) else item.id
         return self._get_object(ItemResp, "item", obj_id, None)
-    
-    def _get_specific_item(self, item: _m.Item | _m.ItemID, typ: type[_m.ItemT] ) -> _m.ItemT:
+
+    def _get_specific_item(self, item: _m.Item | _m.ItemID, typ: type[_m.ItemT]) -> _m.ItemT:
         obj = self.get_item(item)
         if not isinstance(obj, typ):
             raise Exception("invalid item type")
         return obj
-    
+
     def get_item_login(self, item: _m.Item | _m.ItemID):
         return self._get_specific_item(item, _m.ItemLogin)
 
     def get_item_card(self, item: _m.Item | _m.ItemID):
         return self._get_specific_item(item, _m.ItemCard)
-    
+
     def get_item_securenote(self, item: _m.Item | _m.ItemID):
         return self._get_specific_item(item, _m.ItemSecureNote)
-    
+
     def get_item_identity(self, item: _m.Item | _m.ItemID):
         return self._get_specific_item(item, _m.ItemIdentity)
-
 
     def get_items(
         self,
@@ -205,10 +216,11 @@ class Client:
             url=url,
             trash=trash,
         )
-        
+
         return self._get_object_list(ItemsResp, "items", params, exact)
 
-    def _get_specific_items(self,
+    def _get_specific_items(
+        self,
         search: str | None,
         org_id: _m.OrgID | None,
         coll_id: _m.CollID | None,
@@ -217,10 +229,10 @@ class Client:
         trash: bool,
         exact: bool,
         typ: type[_m.ItemT],
-         ) -> list[_m.ItemT]:
+    ) -> list[_m.ItemT]:
         objs = self.get_items(search, org_id, coll_id, folder_id, url, trash, exact)
         return [obj for obj in objs if isinstance(obj, typ)]
-    
+
     def get_item_logins(
         self,
         search: str | None = None,
@@ -232,7 +244,7 @@ class Client:
         exact: bool = False,
     ):
         return self._get_specific_items(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemLogin)
-    
+
     def get_item_cards(
         self,
         search: str | None = None,
@@ -244,7 +256,7 @@ class Client:
         exact: bool = False,
     ):
         return self._get_specific_items(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemCard)
-    
+
     def get_item_identities(
         self,
         search: str | None = None,
@@ -256,7 +268,7 @@ class Client:
         exact: bool = False,
     ):
         return self._get_specific_items(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemIdentity)
-    
+
     def get_item_securenotes(
         self,
         search: str | None = None,
@@ -268,7 +280,6 @@ class Client:
         exact: bool = False,
     ):
         return self._get_specific_items(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemSecureNote)
-
 
     def find_item(
         self,
@@ -288,7 +299,7 @@ class Client:
                 return res[0]
             case _:
                 raise Exception("multiple items matches")
-    
+
     def _find_specific_item(
         self,
         search: str | None,
@@ -298,7 +309,7 @@ class Client:
         url: str | None,
         trash: bool,
         exact: bool,
-        typ: type[_m.ItemT]
+        typ: type[_m.ItemT],
     ) -> _m.ItemT:
         res = self._get_specific_items(search, org_id, coll_id, folder_id, url, trash, exact, typ)
         match len(res):
@@ -308,7 +319,7 @@ class Client:
                 return res[0]
             case _:
                 raise Exception("multiple items matches")
-    
+
     def find_item_login(
         self,
         search: str | None,
@@ -332,7 +343,7 @@ class Client:
         exact: bool,
     ):
         return self._find_specific_item(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemCard)
-    
+
     def find_item_identity(
         self,
         search: str | None,
@@ -344,7 +355,7 @@ class Client:
         exact: bool,
     ):
         return self._find_specific_item(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemIdentity)
-    
+
     def find_item_securenote(
         self,
         search: str | None,
@@ -357,7 +368,6 @@ class Client:
     ):
         return self._find_specific_item(search, org_id, coll_id, folder_id, url, trash, exact, _m.ItemSecureNote)
 
-
     def put_item(self, item: _m.ItemT):
         return self._put(ItemResp, f"/object/item/{item.id}", params=None, payload=item)
 
@@ -367,7 +377,7 @@ class Client:
     def del_item(self, item: _m.Item | _m.ItemID):
         obj_id = item if isinstance(item, str) else item.id
         self._delete(f"/object/item/{obj_id}", params=None)
-        
+
     def restore_item(self, item: _m.Item | _m.ItemID):
         obj_id = item if isinstance(item, str) else item.id
         res = self._post(f"/restore/item/{obj_id}", params=None, payload=None)
@@ -382,10 +392,8 @@ class Client:
         return self._get_object(FolderResp, "folder", obj_id, None)
 
     def get_folders(self, search: str | None = None, exact: bool = False):
-        params = _m.FoldersQuery(
-            search=search
-        )
-        
+        params = _m.FoldersQuery(search=search)
+
         return self._get_object_list(FoldersResp, "folders", params, exact)
 
     def find_folder(self, search: str | None = None, exact: bool = False):
@@ -399,7 +407,7 @@ class Client:
                 raise Exception("multiple folders matches")
 
     def put_folder(self, obj: _m.Folder):
-        return self._put(FolderResp, f"/object/folder/{obj.id}", params=None, payload=obj)        
+        return self._put(FolderResp, f"/object/folder/{obj.id}", params=None, payload=obj)
 
     def post_folder(self, obj: _m.NewFolder):
         return self._post_object(FolderResp, "/object/folder", params=None, payload=obj)
@@ -407,7 +415,6 @@ class Client:
     def del_folder(self, obj: _m.Folder | _m.FolderID):
         obj_id = obj if isinstance(obj, str) else obj.id
         self._delete(f"/object/folder/{obj_id}", params=None)
-
 
     # endregion
 
@@ -418,9 +425,7 @@ class Client:
         return self._get_object(OrgResp, "organization", obj_id, None)
 
     def get_organizations(self, search: str | None = None, exact: bool = False):
-        params = _m.OrganizationsQuery(
-            search=search
-        )
+        params = _m.OrganizationsQuery(search=search)
         return self._get_object_list(OrgsResp, "organizations", params, exact)
 
     def find_organization(
@@ -446,10 +451,7 @@ class Client:
         return self._get_object(CollResp, "collection", obj_id, None)
 
     def get_collections(self, search: str | None = None, org_id: _m.OrgID | None = None, exact: bool = False):
-        params = _m.CollectionsQuery(
-            search=search,
-            org_id=org_id
-        )
+        params = _m.CollectionsQuery(search=search, org_id=org_id)
         endpoint = "collections" if params.org_id is None else "org-collections"
         return self._get_object_list(CollsResp, endpoint, params, exact)
 
@@ -469,22 +471,18 @@ class Client:
                 raise Exception("multiple collections matches")
 
     def post_collection(self, obj: _m.NewCollection):
-        params = _m.OrgCollectionQuery(
-            org_id=obj.org_id
-        )
-        return self._post_object(CollResp,"/object/org-collection", params=params, payload=obj)
-        
+        params = _m.OrgCollectionQuery(org_id=obj.org_id)
+        return self._post_object(CollResp, "/object/org-collection", params=params, payload=obj)
+
     def put_collection(self, obj: _m.Collection):
-        params = _m.OrgCollectionQuery(
-            org_id=obj.org_id
-        )
+        params = _m.OrgCollectionQuery(org_id=obj.org_id)
         return self._put(CollResp, f"/object/org-collection/{obj.id}", params=params, payload=obj)
 
     def del_collection(self, obj: _m.Collection):
-        params = _m.OrgCollectionQuery(
-            org_id=obj.org_id
-        )
+        params = _m.OrgCollectionQuery(org_id=obj.org_id)
         self._delete(f"/object/org-collection/{obj.id}", params=params)
-        
 
     # endregion
+
+
+__all__ = ["DBStatus", "Client", "LinkTarget", "Match"]
